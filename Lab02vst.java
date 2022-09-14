95c7f0dd8721c26539f61605dac176c2a3752743
NickHeinzinger <nicholas.heinzinger@vtsd.com>
2022-09-14 09:18:20 -0400

    100 Point Verstion

--- Lab02vst.java
+++ Lab02vst.java
@@ -8,7 +8,19 @@
 
   public class Lab02vst {
 
-    public static void main(String args[]){
-
+    public static void main(String args[])
+    {
+      System.out.println("Lab 02, 100 Point Version");
+      System.out.println();
+      System.out.println("Measure today's knowledge ");
+      System.out.println("by yesterdays confusion");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+      System.out.println("* ***** *****     ***** ***** * *   * ***** *****");
+      System.out.println("* *   * *   *     *   * *   * * **  *   *   *    ");
+      System.out.println("* *   * *   *     ***** *   * * * * *   *   *****");
+      System.out.println("* *   * *   *     *     *   * * *  **   *       *");
+      System.out.println("* ***** *****     *     ***** * *   *   *   *****");
     }
   }
